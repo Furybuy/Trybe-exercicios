eae6b64cc09e1f800329bd16f73181af7ea64e79
Diego de Almeida <santanafisico@gmail.com>
2022-09-28 18:24:35 -0300

    Exercicio do dia

---
+++ fundamentos/secao-5-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
@@ -0,0 +1,15 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = 'blue';
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = 'purple';
+
+document.getElementById("container").style.backgroundColor = 'black';
+
+document.getElementsByClassName("urgent")[0].style.backgroundColor = 'purple';
+document.getElementsByClassName("urgent")[1].style.backgroundColor = 'purple';
+
+document.getElementsByClassName("non-urgent")[0].style.backgroundColor = 'blue';
+document.getElementsByClassName("non-urgent")[1].style.backgroundColor = 'blue';
